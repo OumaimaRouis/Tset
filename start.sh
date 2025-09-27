488a9cbf38f67233125cf0a7b3c0d7d75ba143eb
OumaimaRouis <oumaima.rouis01@gmail.com>
2025-09-27 14:00:24 +0200

    4th commit

--- start.sh
+++ start.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Se placer dans le dossier backend
 cd backend
-
-# Installer les dépendances Python
 pip install -r requirements.txt
-
-# Lancer Django avec gunicorn
-# Le $PORT est fourni automatiquement par Railway
 gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
