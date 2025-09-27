aac6eb902b13baeeb00fea0ad5f9388e505a61de
OumaimaRouis <oumaima.rouis01@gmail.com>
2025-09-27 16:59:42 +0200

    8th commit

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 cd backend
-pip install -r requirements.txt
-gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
+python3 -m ensurepip --upgrade
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 -m gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
